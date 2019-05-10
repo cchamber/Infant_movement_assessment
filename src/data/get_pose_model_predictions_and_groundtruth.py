37455afddd9b264cf7463c9d5f4ea0f174f90143
cchamber <chambers.claire@gmail.com>
2019-05-10 11:23:09 -0400

    first commit

---
+++ src/data/get_pose_model_predictions_and_groundtruth.py
@@ -0,0 +1,73 @@
+import glob
+import pandas as pd
+import os
+import json
+from util import get_prediction, get_image_meta_data_from_filenames, get_ground_truth, get_keypoints
+import itertools
+import numpy as np
+from itertools import chain
+import cv2
+import matplotlib.pyplot as plt
+
+
+def get_groundtruth_and_predictions(save_images, image_path, gt_path, keras_weights_files, model_names):
+    
+    image_df = get_image_meta_data_from_filenames(image_path)
+    # get ground truth: keypoints and bounding box 
+    gt_df = get_ground_truth(gt_path)
+
+    kp_gt_df = gt_df.groupby('id').apply(get_keypoints).reset_index()[['id', 'part_label', 'x_gt', 'y_gt']]
+    kp_gt_df['person_idx'] = kp_gt_df['id'].astype(str).str[-2:].astype(int)
+    kp_gt_df['id'] = kp_gt_df['id'].astype(str).str[:-2].astype(int)
+    kp_gt_df.loc[kp_gt_df.x_gt==0, 'x_gt'] = np.nan
+    kp_gt_df.loc[kp_gt_df.y_gt==0, 'y_gt'] = np.nan
+
+    bbox_df = gt_df.bbox.apply(pd.Series); bbox_df.columns = ['left', 'bottom', 'width', 'height']
+    bbox_df['id'] = gt_df['id'].astype(str).str[:-2].astype(int)
+    bbox_df['bbox_diag'] = np.sqrt(bbox_df.width**2+bbox_df.height**2)
+    kp_gt_df = pd.merge(kp_gt_df, bbox_df[['id', 'bbox_diag']], on='id', how='left')
+    kp_gt_df.loc[kp_gt_df.x_gt==0, 'x_gt'] = np.nan
+    kp_gt_df.loc[kp_gt_df.y_gt==0, 'y_gt'] = np.nan
+
+    # get prediction
+    pred_df = pd.DataFrame()
+    for i, (iweight, imodel) in enumerate(zip(keras_weights_files, model_names)):
+        zdf = get_prediction(iweight, imodel, save_images, image_path)
+        if i ==0:
+            pred_df = zdf.reset_index()
+        elif i>0:
+            pred_df = pd.merge(pred_df, zdf.reset_index(), on=['id', 'part_label'], how='outer')
+
+    columns = pd.MultiIndex.from_product([['ground_truth'],['x','y']], names=['var_type','dim'])
+    index = pd.MultiIndex.from_arrays([np.array(kp_gt_df.id), np.array(kp_gt_df.part_label)], names=['id','part_label'])
+    gt_df_merge = pd.DataFrame(kp_gt_df[['x_gt', 'y_gt']].as_matrix(),index=index, columns=columns).reset_index()
+
+    gt_pred_df = pd.merge(gt_df_merge, pred_df, on=['id','part_label'], how = 'outer')
+
+    bbox = bbox_df[['id','bbox_diag']]
+    columns = pd.MultiIndex.from_arrays([['id','ground_truth'],['','bbox_diag']], names=['var_type','dim'])
+    bbox.columns = columns
+    gt_pred_df = pd.merge(gt_pred_df, bbox, on='id', how='outer')
+    gt_pred_df['frame', ''] = gt_pred_df['id', ''].astype(str).str[7:]
+    gt_pred_df['video', ''] = gt_pred_df['id', ''].astype(str).str[1:7]
+
+    # compute distance
+    for i in model_names:
+        gt_pred_df[i, 'distance_pix'] = np.sqrt((gt_pred_df[i].x - gt_pred_df.ground_truth.x)**2 + (gt_pred_df[i].y-gt_pred_df.ground_truth.y)**2) 
+        gt_pred_df[i, 'distance_norm'] = gt_pred_df[i, 'distance_pix']/gt_pred_df['ground_truth','bbox_diag']
+    return gt_pred_df
+
+
+def main():
+    save_images = 1
+    image_path = '/data2/clairec/infant_NN_training_dataset/val_all_100818_1inf_step1'
+    gt_path = '/data2/clairec/infant_NN_training_dataset/person_keypoints_val_all_100818_1inf_step1.json'
+    keras_weights_files = ['../../models/cmu_model.h5', '../../models/trained_model_oct.h5']
+    model_names = ['original_model', 'trained_model']
+
+    gt_pred_df = get_groundtruth_and_predictions(save_images, image_path, gt_path, keras_weights_files, model_names)
+
+    gt_pred_df.to_pickle('../../data/interim/model_predictions_and_groundtruth.pkl')
+
+if __name__ == '__main__':
+    main()
---
+++ src/data/model.py
@@ -0,0 +1,203 @@
+from keras.models import Model
+from keras.layers.merge import Concatenate
+from keras.layers import Activation, Input, Lambda
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import Multiply
+from keras.regularizers import l2
+from keras.initializers import random_normal, constant
+
+import re
+
+
+#stages = 6
+#np_branch1 = 38
+#np_branch2 = 19
+
+def relu(x): return Activation('relu')(x)
+
+def conv(x, nf, ks, name, weight_decay):
+    kernel_reg = l2(weight_decay[0]) if weight_decay else None
+    bias_reg = l2(weight_decay[1]) if weight_decay else None
+
+    x = Conv2D(nf, (ks, ks), padding='same', name=name,
+               kernel_regularizer=kernel_reg,
+               bias_regularizer=bias_reg,
+               kernel_initializer=random_normal(stddev=0.01),
+               bias_initializer=constant(0.0))(x)
+    return x
+
+def pooling(x, ks, st, name):
+    x = MaxPooling2D((ks, ks), strides=(st, st), name=name)(x)
+    return x
+
+def vgg_block(x, weight_decay):
+    # Block 1
+    x = conv(x, 64, 3, "conv1_1", (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 64, 3, "conv1_2", (weight_decay, 0))
+    x = relu(x)
+    x = pooling(x, 2, 2, "pool1_1")
+
+    # Block 2
+    x = conv(x, 128, 3, "conv2_1", (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 3, "conv2_2", (weight_decay, 0))
+    x = relu(x)
+    x = pooling(x, 2, 2, "pool2_1")
+
+    # Block 3
+    x = conv(x, 256, 3, "conv3_1", (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 256, 3, "conv3_2", (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 256, 3, "conv3_3", (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 256, 3, "conv3_4", (weight_decay, 0))
+    x = relu(x)
+    x = pooling(x, 2, 2, "pool3_1")
+
+    # Block 4
+    x = conv(x, 512, 3, "conv4_1", (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 512, 3, "conv4_2", (weight_decay, 0))
+    x = relu(x)
+
+    # Additional non vgg layers
+    x = conv(x, 256, 3, "conv4_3_CPM", (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 3, "conv4_4_CPM", (weight_decay, 0))
+    x = relu(x)
+
+    return x
+
+
+def stage1_block(x, num_p, branch, weight_decay):
+    # Block 1
+    x = conv(x, 128, 3, "Mconv1_stage1_L%d" % branch, (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 3, "Mconv2_stage1_L%d" % branch, (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 3, "Mconv3_stage1_L%d" % branch, (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 512, 1, "Mconv4_stage1_L%d" % branch, (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, num_p, 1, "Mconv5_stage1_L%d" % branch, (weight_decay, 0))
+
+    return x
+
+
+def stageT_block(x, num_p, stage, branch, weight_decay):
+    # Block 1
+    x = conv(x, 128, 7, "Mconv1_stage%d_L%d" % (stage, branch), (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv2_stage%d_L%d" % (stage, branch), (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv3_stage%d_L%d" % (stage, branch), (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv4_stage%d_L%d" % (stage, branch), (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv5_stage%d_L%d" % (stage, branch), (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, 128, 1, "Mconv6_stage%d_L%d" % (stage, branch), (weight_decay, 0))
+    x = relu(x)
+    x = conv(x, num_p, 1, "Mconv7_stage%d_L%d" % (stage, branch), (weight_decay, 0))
+
+    return x
+
+
+def apply_mask(x, mask1, mask2, num_p, stage, branch, np_branch1, np_branch2):
+    w_name = "weight_stage%d_L%d" % (stage, branch)
+
+    # TODO: we have branch number here why we made so strange check
+    assert np_branch1 != np_branch2 # we selecting branches by number of pafs, if they accidentally became the same it will be disaster
+
+    if num_p == np_branch1:
+        w = Multiply(name=w_name)([x, mask1])  # vec_weight
+    elif num_p == np_branch2:
+        w = Multiply(name=w_name)([x, mask2])  # vec_heat
+    else:
+        assert False, "wrong number of layers num_p=%d " % num_p
+    return w
+
+
+
+def get_lrmult(model):
+
+    # setup lr multipliers for conv layers
+    lr_mult = dict()
+
+    for layer in model.layers:
+
+        if isinstance(layer, Conv2D):
+
+            # stage = 1
+            if re.match("Mconv\d_stage1.*", layer.name):
+                kernel_name = layer.weights[0].name
+                bias_name = layer.weights[1].name
+                lr_mult[kernel_name] = 1
+                lr_mult[bias_name] = 2
+
+            # stage > 1
+            elif re.match("Mconv\d_stage.*", layer.name):
+                kernel_name = layer.weights[0].name
+                bias_name = layer.weights[1].name
+                lr_mult[kernel_name] = 4
+                lr_mult[bias_name] = 8
+
+            # vgg
+            else:
+               print("matched as vgg layer", layer.name)
+               kernel_name = layer.weights[0].name
+               bias_name = layer.weights[1].name
+               lr_mult[kernel_name] = 1
+               lr_mult[bias_name] = 2
+
+    return lr_mult
+
+
+def get_testing_model(np_branch1=38, np_branch2=19, stages = 6):
+
+    img_input_shape = (None, None, 3)
+
+    img_input = Input(shape=img_input_shape)
+
+    img_normalized = Lambda(lambda x: x / 256 - 0.5)(img_input) # [-0.5, 0.5]
+
+    # VGG
+    stage0_out = vgg_block(img_normalized, None)
+
+    stages_out = []
+
+    # stage 1 - branch 1 (PAF)
+    if np_branch1 > 0:
+        stage1_branch1_out = stage1_block(stage0_out, np_branch1, 1, None)
+        stages_out.append(stage1_branch1_out)
+
+    # stage 1 - branch 2 (confidence maps)
+    if np_branch2 > 0:
+        stage1_branch2_out = stage1_block(stage0_out, np_branch2, 2, None)
+        stages_out.append(stage1_branch2_out)
+
+    x = Concatenate()(stages_out + [stage0_out])
+
+    # stage t >= 2
+    stageT_branch1_out = None
+    stageT_branch2_out = None
+    for sn in range(2, stages + 1):
+
+        stages_out = []
+
+        if np_branch1 > 0:
+            stageT_branch1_out = stageT_block(x, np_branch1, sn, 1, None)
+            stages_out.append(stageT_branch1_out)
+        if np_branch2 > 0:
+            stageT_branch2_out = stageT_block(x, np_branch2, sn, 2, None)
+            stages_out.append(stageT_branch2_out)
+
+        if sn < stages:
+            x = Concatenate()(stages_out + [stage0_out])
+
+    model = Model(inputs=[img_input], outputs=[stageT_branch1_out, stageT_branch2_out])
+
+    return model
